97b74d7999c62d90c4c7504ef8725b5f32f3b2c9
WilliamWaly <184936@stud.hvl.no>
2024-09-26 20:29:31 +0200

    dfs

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -15,21 +15,17 @@ public class Matriser {
 
 	
    // b)
-	    public static String tilStreng(int[][] matrise) {
-	        StringBuilder sb = new StringBuilder(); 
-	        
-	        for (int i = 0; i < matrise.length; i++) { 
-	            for (int j = 0; j < matrise[i].length; j++) { 
-	                sb.append(matrise[i][j]); 
-	                if (j < matrise[i].length - 1) { 
-	                    sb.append(" ");
-	                }
-	            }
-	            sb.append(" \n");
+	public static String tilStreng(int[][] matrise) {
+	    StringBuilder sb = new StringBuilder();
+	    for (int rad = 0; rad < matrise.length; rad++) {
+	        for (int kol = 0; kol < matrise[rad].length; kol++) {
+	            sb.append(matrise[rad][kol]).append(" ");
 	        }
-
-	        return sb.toString(); 
+	        sb.append("\n");
 	    }
+	    return sb.toString();
+	}
+
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
