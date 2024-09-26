234a425278a5da8c3276022bff3ecc451b998cc4
WilliamWaly <184936@stud.hvl.no>
2024-09-26 11:49:24 +0200

    dg

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -31,26 +31,18 @@ public class Matriser {
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
+		if (a.length != b.length) return false;
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+		for (int i = 0; i < a.length; i++) {
+			if (a[i].length != b[i].length) return false;
+			for (int j = 0; j < a[i].length; j++) {
+		    if (a[i][j] != b[i][j]) return false;
+		        }
+		    }
+
+		    return true;
+		}
+
 		
 	}
 	
-	// e)
-	public static int[][] speile(int[][] matrise) {
-
-		// TODO
-
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
-	}
-
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
-	}
-}
